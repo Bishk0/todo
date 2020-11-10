a37802a8e66094b28c8517f7f10ee30ac9b556c3
Bishko <yurik322412@gmail.com>
2020-11-10 19:06:46 +0200

    input test data in components

---
+++ src/app/services/data-handler.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { TestData } from '../data/TestData';
+import { Category } from '../models/Category';
+import { Task } from '../models/Task';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataHandlerService {
+  constructor() {}
+
+  getCategories(): Category[] {
+    return TestData.categories;
+  }
+
+  getTasks(): Task[] {
+    return TestData.tasks;
+  }
+}
---
+++ src/app/views/categories/categories.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/models/Category';
+import { Task } from 'src/app/models/Task';
+import { DataHandlerService } from 'src/app/services/data-handler.service';
+
+@Component({
+  selector: 'app-categories',
+  templateUrl: './categories.component.html',
+  styleUrls: ['./categories.component.scss'],
+})
+export class CategoriesComponent implements OnInit {
+  categories: Category[];
+
+  constructor(private dataHandler: DataHandlerService) {}
+
+  ngOnInit(): void {
+    this.categories = this.dataHandler.getCategories();
+  }
+}
