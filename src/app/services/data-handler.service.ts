a30e0847380fc3a9034b6927377a4ae3c2b94b4f
Bishko <yurik322412@gmail.com>
2020-11-11 22:35:11 +0200

    get tasks by category

--- src/app/services/data-handler.service.ts
+++ src/app/services/data-handler.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { TestData } from '../data/TestData';
 import { Category } from '../models/Category';
 import { Task } from '../models/Task';
@@ -7,13 +8,23 @@ import { Task } from '../models/Task';
   providedIn: 'root',
 })
 export class DataHandlerService {
-  constructor() {}
+  tasksSubject = new BehaviorSubject<Task[]>(TestData.tasks);
+  categoriesSubject = new BehaviorSubject<Category[]>(TestData.categories);
 
-  getCategories(): Category[] {
-    return TestData.categories;
+  constructor() {
+    this.getTasks();
   }
 
-  getTasks(): Task[] {
-    return TestData.tasks;
+  // getCategories(): Category[] {
+  //   return TestData.categories;
+  // }
+
+  getTasks() {
+    this.tasksSubject.next(TestData.tasks);
+  }
+
+  getTasksByCategory(category: Category) {
+    const tasks = TestData.tasks.filter((task) => task.category === category);
+    this.tasksSubject.next(tasks);
   }
 }
